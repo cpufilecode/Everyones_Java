3fab8486d1b99858875ce1fc14ea53441536c29b
cpufilecode <zf0908@naver.com>
2024-03-23 23:49:33 +0900

    upload

---
+++ src/ch02/ConstantExam.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class ConstantExam {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        int radius = 5;
+        double result = radius * 2 * PI;
+        System.out.println(result);
+    }
+}

694f4c5d6264189972846a060099babb2f4c3634
cpufilecode <zf0908@naver.com>
2024-03-24 23:45:50 +0900

    upload

---
+++ src/ch02/PrimitiveDataTypeExam.java
@@ -0,0 +1,23 @@
+package ch02;
+
+public class PrimitiveDataTypeExam {
+    public static void main(String[] args) {
+        boolean isFun = true;
+        System.out.println(isFun);
+
+        char c = 'f';
+        System.out.println(c);
+
+        int x = 59;
+        System.out.println(x);
+
+        long bing = 345455433L;
+        System.out.println(bing);
+
+        float f = 32.f;
+        System.out.println(f);
+
+        double d = 3434343.5;
+        System.out.println(d);
+    }
+}

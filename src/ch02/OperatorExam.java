ad5f7625d694171a98de74bac42dbe9e2e015515
cpufilecode <zf0908@naver.com>
2024-03-24 23:51:42 +0900

    upload

--- src/ch02/OperatorExam.java
+++ src/ch02/OperatorExam.java
@@ -3,6 +3,10 @@ package ch02;
 public class OperatorExam {
     public static void main(String[] args) {
         int i1 = -5;
+        int i2 = +i1;
+        int i3 = -i1;
         System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
     }
 }

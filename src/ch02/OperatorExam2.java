e479bafd45ededb918750f8ef79949979230a096
cpufilecode <zf0908@naver.com>
2024-03-25 23:36:46 +0900

    upload

---
+++ src/ch02/OperatorExam2.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class OperatorExam2 {
+    public static void main(String[] args) {
+        int i=10;
+        int j=10;
+
+        System.out.println(i==j);
+        System.out.println(i!=j);
+    }
+}

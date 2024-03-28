1a67ac0b76397d753a24d4e13209ae3d3f25a7bf
cpufilecode <zf0908@naver.com>
2024-03-28 14:05:17 +0900

    upload

---
+++ src/ch03/ForExam.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class ForExam {
+    public static void main(String[] args) {
+        int total = 0;
+        for(int i=1; i<=100; i++) {
+            total = total+i;
+            if(i==50) {
+                break;
+            }
+        }
+        System.out.println(total);
+    }
+}

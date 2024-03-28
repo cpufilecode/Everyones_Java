8fdee6d07cad3a17a7159d043391550a3eb63da8
cpufilecode <zf0908@naver.com>
2024-03-28 10:31:48 +0900

    upload

--- src/ch03/SwitchExam.java
+++ src/ch03/SwitchExam.java
@@ -2,17 +2,35 @@ package ch03;
 
 public class SwitchExam {
     public static void main(String[] args) {
-        int value = 1;
+        int value = 2;
 
         switch (value) {
             case 1:
                 System.out.println("1");
+                break;
             case 2:
                 System.out.println("2");
+                break;
             case 3:
                 System.out.println("3");
+                break;
             default:
                 System.out.println("그 외 다른 숫자");
+        }
+        String str = "A";
+
+        switch (str) {
+            case "A":
+                System.out.println("A");
+                break;
+            case "B":
+                System.out.println("B");
+                break;
+            case "C":
+                System.out.println("C");
+                break;
+            default:
+                System.out.println("그 외 다른 문자");
         }
     }
 }

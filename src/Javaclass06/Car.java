6f09d64a44da671a452a8eca52bec2f598dd05f6
cpufilecode <zf0908@naver.com>
2024-03-29 17:14:13 +0900

    upload

---
+++ src/Javaclass06/Car.java
@@ -0,0 +1,20 @@
+package Javaclass06;
+
+public class Car {
+    String name;
+    int number;
+
+    public Car(String n) {
+        this.name = name;
+    }
+
+    public Car() {
+        this.name = "이름없음";
+        this.number = 0;
+    }
+
+    public Car(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+}
---
+++ src/Javaclass06/MyClass2.java
@@ -0,0 +1,14 @@
+package Javaclass06;
+
+public class MyClass2 {
+    public String plus(String x, String y) {
+        return x+y;
+    }
+    public int plus (int x, int y, int z) {
+        return x+y+z;
+    }
+
+    public int plus(int x, int y) {
+        return x + y;
+    }
+}

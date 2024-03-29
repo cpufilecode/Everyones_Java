9eacfdb52e2dad319bda159adc32fbd2658cf698
cpufilecode <zf0908@naver.com>
2024-03-29 10:37:07 +0900

    upload

---
+++ src/ch04/ForEachExam.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class ForEachExam {
+    public static void main(String[] args) {
+        int[] iarr ={10,20,30,40,50};
+
+        for(int value:iarr) {
+            System.out.println(value);
+        }
+    }
+}
